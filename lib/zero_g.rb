e97d05d6f5323077e74655950383bc4dea305d00
Thane Vo <vothane@gmail.com>
2015-12-24 00:49:30 +0000

    implement partial, trampoline and lazy sequence methods

--- lib/zero_g.rb
+++ lib/zero_g.rb
@@ -1,5 +1,35 @@
 require "zero_g/version"
+require "ostruct"
 
 module ZeroG
-  # Your code goes here...
+
+  def self.partial(fn, *args_partial)
+    lambda {|*args_lambda| fn.call(*args_partial.concat(args_lambda))}
+  end
+
+  def self.trampoline(fn)
+    while fn.instance_of?(Proc) do
+      fn = fn.call
+    end
+    return fn
+  end
+
+  def self.lazy(coll, first_fn, rest_fn)
+    return nil if coll.nil?
+
+    lazy_seq = OpenStruct.new
+
+    lazy_seq.first = lambda do
+      first_fn.call(coll)
+    end
+
+    # Unrealized & lazy recursive calls through partial method. When realized recursion
+    # will be not be used but instead by trampolining (emulating tail-recursion).
+    lazy_seq.rest = lambda do
+      tail = self.partial(method(:lazy), rest_fn.call(coll), first_fn, rest_fn)
+      return trampoline(tail)
+    end
+
+    return lazy_seq
+  end
 end
--- test/zero_g_test.rb
+++ test/zero_g_test.rb
@@ -1,11 +1,16 @@
-require 'test_helper'
+require_relative 'test_helper'
 
 class ZeroGTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::ZeroG::VERSION
-  end
-
-  def test_it_does_something_useful
-    assert false
+  def test_lazy
+    first = lambda {|arr| return arr.first}
+    rest = lambda {|arr| return arr.drop(1)}
+    lz = ZeroG.lazy([1, 2, 3], first, rest)
+    assert_equal 1, lz.first.call
+    lz = lz.rest.call
+    assert_instance_of Proc, lz.rest #seq must still be lazy
+    assert_equal 2, lz.first.call
+    lz = lz.rest.call
+    assert_instance_of Proc, lz.rest #seq must still be lazy
+    assert_equal 3, lz.first.call
   end
 end
